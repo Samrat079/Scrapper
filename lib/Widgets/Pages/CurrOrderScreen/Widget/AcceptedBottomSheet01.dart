a8091ad883ea61314dcbc6f82f5a13d0c69665f3
samrat079 <samratdutta079@gmail.com>
2026-05-08 13:16:05 +0530

    Added phone calling

--- lib/Widgets/Pages/CurrOrderScreen/Widget/AcceptedBottomSheet01.dart
+++ lib/Widgets/Pages/CurrOrderScreen/Widget/AcceptedBottomSheet01.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:duration/duration.dart';
 import 'package:flutter/material.dart';
 import 'package:scrapper/Widgets/Custome/CardList01/CardList01.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../Models/Orders/Order01.dart';
 import '../../../../Services/OrderServices/OrderService01.dart';
@@ -21,78 +22,84 @@ class AcceptedBottomSheet01 extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => SafeArea(
-    child: CenterColumn04(
-      scrollController: controller,
-      children: [
-        /// Sanitarian info
-        ListTile(
-          leading: CachedNetworkImage(
-            imageUrl: order.sanitarian!.photoUrl,
-            imageBuilder: (context, provider) =>
-                CircleAvatar(backgroundImage: provider),
-            placeholder: (context, url) => Icon(Icons.person_outline),
-            errorWidget: (context, url, error) => Icon(Icons.error_outline),
-          ),
-          title: Text(
-            "${order.sanitarian!.displayName} is has accepted your order",
-          ),
-          subtitle: Text(
-            "Reaching your destination in ${order.routesRes.duration.pretty(maxUnits: 2, tersity: DurationTersity.minute)}",
-          ),
-        ),
-        Divider(),
-        context.gapMD,
+  Widget build(BuildContext context) =>
+      SafeArea(
+        child: CenterColumn04(
+          scrollController: controller,
+          children: [
 
-        /// Otp
-        ListTile(
-          visualDensity: VisualDensity(vertical: 2),
-          leading: Icon(Icons.lock),
-          title: Text("Otp"),
-          subtitle: Text(
-            "Share this otp with your sanitarian at your door step.",
-          ),
-          trailing: Card(
-            color: context.colorScheme.primaryContainer,
-            child: Padding(
-              padding: context.paddingMD,
-              child: Text(
-                order.otp.toString(),
-                style: TextStyle(color: context.colorScheme.onPrimary),
+            /// Sanitarian info
+            ListTile(
+              leading: CachedNetworkImage(
+                imageUrl: order.sanitarian!.photoUrl,
+                imageBuilder: (context, provider) =>
+                    CircleAvatar(backgroundImage: provider),
+                placeholder: (context, url) => Icon(Icons.person_outline),
+                errorWidget: (context, url, error) => Icon(Icons.error_outline),
+              ),
+              title: Text(
+                "${order.sanitarian!.displayName} is has accepted your order",
+              ),
+              subtitle: Text(
+                "Reaching your destination in ${order.routesRes.duration.pretty(
+                    maxUnits: 2, tersity: DurationTersity.minute)}",
+              ),
+              trailing: IconButton(onPressed: () =>
+                  launchUrl(Uri.parse("tel:${order.customer.phoneNumber}")),
+                  icon: Icon(Icons.call)),
+            ),
+            Divider(),
+            context.gapMD,
+
+            /// Otp
+            ListTile(
+              visualDensity: VisualDensity(vertical: 2),
+              leading: Icon(Icons.lock),
+              title: Text("Otp"),
+              subtitle: Text(
+                "Share this otp with your sanitarian at your door step.",
+              ),
+              trailing: Card(
+                color: context.colorScheme.primaryContainer,
+                child: Padding(
+                  padding: context.paddingMD,
+                  child: Text(
+                    order.otp.toString(),
+                    style: TextStyle(color: context.colorScheme.onPrimary),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Divider(),
-        context.gapMD,
+            Divider(),
+            context.gapMD,
 
-        /// Order details
-        ListTile(
-          leading: const Icon(Icons.house_outlined),
-          title: Text(order.address.place.name!),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                order.address.place.displayName!,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
+            /// Order details
+            ListTile(
+              leading: const Icon(Icons.house_outlined),
+              title: Text(order.address.place.name!),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    order.address.place.displayName!,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                  Text(order.address.phoneNumber),
+                ],
               ),
-              Text(order.address.phoneNumber),
-            ],
-          ),
-        ),
-        context.gapMD,
+            ),
+            context.gapMD,
 
-        ElevatedButton(
-          onPressed: onCancel,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: context.colorScheme.error,
-            foregroundColor: context.colorScheme.onError,
-          ),
-          child: const Text('Cancel'),
+            ElevatedButton(
+              onPressed: onCancel,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: context.colorScheme.error,
+                foregroundColor: context.colorScheme.onError,
+              ),
+              child: const Text('Cancel'),
+            ),
+          ],
         ),
-      ],
-    ),
-  );
+      );
 }
