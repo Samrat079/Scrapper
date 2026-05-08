d47ed589ba4e3733f6779c1692d5941048190a84
samrat079 <samratdutta079@gmail.com>
2026-05-08 13:56:44 +0530

    added phone option

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:scrapper/Services/AppUserServices/AppUserServices01.dart';
 import 'package:scrapper/Utils/Providers/Providers01.dart';
 import 'package:scrapper/Utils/Router/RouteGen.dart';
 import 'package:scrapper/firebase_options.dart';
